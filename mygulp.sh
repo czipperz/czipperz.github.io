84de5c46a08d9d81dc8a116c0d2ddaf2b9858da7
Czipperz <czipperz@gmail.com>
2015-03-18 20:13:13 -0700

    Made it work (the css map isn't working)

--- mygulp.sh
+++ mygulp.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 #stylus
-stylus -m < stylus/style.styl > style.css
+stylus < stylus/style.styl > style.css #-u autoprefixer-stylus -m stylus/style.styl > style.css
 #haml
-for f in $(ls haml/ | egrep *.haml); do
-	#compiles all the haml files in the haml/ directory into the base directory
-	#as html files.
+for f in $(eval ls haml/ | egrep *.haml); do
+	#compiles all the haml files in the haml/ directory into the base directory as html files.
 	haml "haml/$f" "${f%.haml}.html"
 done
