2e3d172f644b6f1a5674a5738d0d021056ed3cf0
Czipperz <czipperz@gmail.com>
2015-04-03 17:28:57 -0700

    Got the new stuffs

--- chs/mygulp.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-#stylus
-stylus < stylus/compiler.styl > style.css
-#haml
-for f in $(eval ls haml/ | egrep *.haml); do
-	#compiles all the haml files in the haml/ directory into the base directory as html files.
-	haml "haml/$f" "${f%.haml}.html"
-done
