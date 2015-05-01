1104154fc5656006e2a702da60dd6fc6b3ef783d
Czipperz <czipperz@gmail.com>
2015-04-30 17:18:07 -0700

    Sass

--- mygulp.sh
+++ mygulp.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #stylus
-stylus < stylus/style.styl > style.css #-u autoprefixer-stylus -m stylus/style.styl > style.css
+sass sass/style.scss style.css #-u autoprefixer-stylus -m stylus/style.styl > style.css
 #haml
 for f in $(eval ls haml/ | egrep *.haml); do
 	#compiles all the haml files in the haml/ directory into the base directory as html files.
