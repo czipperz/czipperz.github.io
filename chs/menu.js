6acc289c66dac8a3f1283a24a09c368e5ed00d01
Czipperz <czipperz@gmail.com>
2015-04-30 21:38:36 -0700

    Javascript

---
+++ chs/menu.js
@@ -0,0 +1,26 @@
+document.getElementById("top").innerHTML =
+"<div id='header'>"+
+"<h1>"+
+"  <a href='index.html'>Corvallis High School</a>"+
+"</h1>"+
+"</div>"+
+"<div id='navbar'>"+
+"</div>";
+document.getElementById("navbar").innerHTML =
+"<ul>"+
+"	<li>"+
+"		<a href=\"index.html\">Home</a>"+
+"	</li>"+
+"	<li>"+
+"		<a href=\"\">Counseling</a>"+
+"	</li>"+
+"	<li>"+
+"		<a href=\"departments.html\">Departments</a>"+
+"	</li>"+
+"	<li>"+
+"		<a href=\"\">Activities</a>"+
+"	</li>"+
+"	<li>"+
+"		<a href=\"\">Athletics</a>"+
+"	</li"+
+"</ul>";
