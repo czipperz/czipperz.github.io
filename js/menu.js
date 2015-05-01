7cb097a4b38f95363aa879ad6987ed8898bd7764
Czipperz <czipperz@gmail.com>
2015-04-30 21:07:55 -0700

    Menu

---
+++ js/menu.js
@@ -0,0 +1,13 @@
+document.getElementById("menu").innerHTML =
+"<ul>"+
+"	<li>"+
+"		<a href=\"index.html\">Home</a>"+
+"	</li>"+
+"	<li>"+
+"		<a href=\"#projects\">Projects</a>"+
+"	</li>"+
+"	<li>"+
+"		<a href=\"#contact\">Contact</a>"+
+"	</li>"+
+"</ul>";
+
