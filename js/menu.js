a35342e8f249b87516c695eae6936df24d4c9edd
Czipperz <czipperz@gmail.com>
2015-05-17 12:52:01 -0700

    Simpler menu

--- js/menu.js
+++ js/menu.js
@@ -1,12 +1,12 @@
 document.getElementById("menu").innerHTML =
 "<ul>"+
 "	<li>"+
-"		<a href=\"http://czipperz.github.io/index.html\">Home</a>"+
+"		<a href=\"http://czipperz.github.io\">Home</a>"+
 "	</li>"+
 "	<li>"+
-"		<a href=\"http://czipperz.github.io/chs/index.html\">CHS Preview</a>"+
+"		<a href=\"http://czipperz.github.io/chs\">CHS Preview</a>"+
 "	</li>"+
 "	<li>"+
-"		<a href=\"http://czipperz.github.io/hbml/index.html\">HBML</a>"+
+"		<a href=\"http://czipperz.github.io/hbml\">HBML</a>"+
 "	</li>"+
 "</ul>";
