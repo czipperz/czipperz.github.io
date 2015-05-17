3a07be504d4a7b8c95f93a65967eb7fb9b6fc413
Czipperz <czipperz@gmail.com>
2015-05-16 23:00:45 -0700

    Removed contact

--- js/menu.js
+++ js/menu.js
@@ -7,9 +7,6 @@ document.getElementById("menu").innerHTML =
 "		<a href=\"http://czipperz.github.io/chs/index.html\">CHS Preview</a>"+
 "	</li>"+
 "	<li>"+
-"		<a href=\"http://czipperz.github.io/hbml/index.html\">HBML Language</a>"+
-"	</li>"+
-"	<li>"+
-"		<a href=\"#contact\">Contact</a>"+
+"		<a href=\"http://czipperz.github.io/hbml/index.html\">HBML</a>"+
 "	</li>"+
 "</ul>";
