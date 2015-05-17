090296e1aae0dcb488af86fb819168321c3fd429
Czipperz <czipperz@gmail.com>
2015-05-16 22:59:12 -0700

    New menu

--- js/menu.js
+++ js/menu.js
@@ -1,13 +1,15 @@
 document.getElementById("menu").innerHTML =
 "<ul>"+
 "	<li>"+
-"		<a href=\"index.html\">Home</a>"+
+"		<a href=\"http://czipperz.github.io/index.html\">Home</a>"+
 "	</li>"+
 "	<li>"+
-"		<a href=\"#projects\">Projects</a>"+
+"		<a href=\"http://czipperz.github.io/chs/index.html\">CHS Preview</a>"+
+"	</li>"+
+"	<li>"+
+"		<a href=\"http://czipperz.github.io/hbml/index.html\">HBML Language</a>"+
 "	</li>"+
 "	<li>"+
 "		<a href=\"#contact\">Contact</a>"+
 "	</li>"+
 "</ul>";
-
