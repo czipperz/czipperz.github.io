4ae35ca9772302c2b4ee726c19f695d40656f65b
Czipperz <czipperz@gmail.com>
2018-08-31 14:56:32 -0400

    Title bar auto hides on scroll on mobile

--- js/script.js
+++ js/script.js
@@ -34,4 +34,25 @@ $(document).ready(function() {
 
         $this.text(linkText);
     });
+
+    if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
+        /* When the user scrolls down, hide the navbar. When the user scrolls up, show the navbar */
+        var prevScrollpos = window.pageYOffset;
+        var top = 0;
+        window.onscroll = function() {
+            var currentScrollPos = window.pageYOffset;
+            console.log(top);
+            top += prevScrollpos - currentScrollPos;
+            var min = -document.getElementById("topbar").offsetHeight;
+            if (top <= min) {
+                top = min;
+                document.getElementById("topbar").style.visibility = "hidden";
+            } else {
+                document.getElementById("topbar").style.visibility = "visible";
+            }
+            top = Math.min(top, 0);
+            document.getElementById("topbar").style.top = top + "px";
+            prevScrollpos = currentScrollPos;
+        }
+    }
 });
